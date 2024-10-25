dfc01a0b45a2ef9f6db2aa46d85dc7867d868117
dianaestrada <nabeles@outlook.es>
2024-10-24 22:39:28 -0500

    Agrega datos calculados

--- Views/Registro.xaml.cs
+++ Views/Registro.xaml.cs
@@ -2,25 +2,49 @@ namespace destradaExamen.Views;
 
 public partial class Registro : ContentPage
 {
-    string user_global = "";
+    double costo = 1500.0;
+    double porcentajecuota = 0.04;
     public Registro(string user)
 	{
 		InitializeComponent();
         lbluser.Text = "Usuario Conectado " + user;
-        user_global = user;
+        
+    }
+
+      private void btnPagomes_Clicked(object sender, EventArgs e)
+    {
+        
+        if (double.TryParse(txtmontoinicial.Text, out double montoInicial))
+        {
+            if (montoInicial >= 1 && montoInicial <= costo)
+            {
+                double montofaltante = costo - montoInicial;
+
+                double montoCuota = (montofaltante / 4) * (1 + porcentajecuota);
+
+                txtpagomes.Text =$"{montoCuota}";
+            }
+            else
+            {
+                txtpagomes.Text = "El monto inicial debe estar entre 1 y 1500.";
+            }
+        }
+        else
+        {
+            txtpagomes.Text = "Por favor, ingrese un monto válido.";
+        }
+         
     }
 
     private void btnResumen_Clicked(object sender, EventArgs e)
     {
-        Navigation.PushAsync(new Resumen(user_global));
+        int monto = int.Parse(txtmontoinicial.Text);
+        string fecha = dFechas.Date.ToString();
+        string pais = pkpais.Items[pkpais.SelectedIndex].ToString();
+        string ciudad = pkciudades.Items[pkciudades.SelectedIndex].ToString();
+        double montofaltante = costo - monto;
+        double pagoMensual = (montofaltante / 4) * (1 + porcentajecuota);
+        Navigation.PushAsync(new Resumen(txtnombre.Text, txtapellido.Text, txtedad.Text, fecha, pais, ciudad, monto, pagoMensual));
     }
 
-    private void btnPagomes_Clicked(object sender, EventArgs e)
-    {
-        double montoInicial = double.Parse(txtpagomes.Text);
-        double pagoMensual = (1500 - montoInicial) / 4 * 1.04; 
-       
-
-
-    }
 }
--- Views/Resumen.xaml.cs
+++ Views/Resumen.xaml.cs
@@ -1,9 +1,22 @@
+using Microsoft.Maui.Graphics;
+
 namespace destradaExamen.Views;
 
 public partial class Resumen : ContentPage
 {
-    public Resumen(string usuario)
+    public Resumen(string nombre1, string apellido1, string edad1, string fecha1, string pais1, string ciudad1, double montoInicial, 
+        double pagoMensual)
 	{
 		InitializeComponent();
+        nombre.Text = nombre1;
+        apellido.Text = apellido1;
+        edad.Text = edad1;
+        fecha.Text = fecha1;
+        pais.Text = pais1;
+        lblciudad.Text = ciudad1;
+        lblmonto.Text = $"{montoInicial}";
+        lblpagomes.Text = $"{pagoMensual}";
+        lblpagototal.Text = $"{pagoMensual*4+montoInicial}";
+
     }
 }
